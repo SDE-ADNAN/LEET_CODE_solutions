a1c85cc0cdf2a3821d5bf39d22200f360cd1646b
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-07-29 23:22:51 +0530

    added solution for 890th soln of leetcode

--- src/com/adnan/_890_Find_and_Replace_Pattern.java
+++ src/com/adnan/_890_Find_and_Replace_Pattern.java
@@ -40,11 +40,45 @@ pattern and words[i] are lowercase English letters.
 
   */
 
+
+ import java.util.*;
 public class _890_Find_and_Replace_Pattern {
     public static void main(String[] args) {
-        String[] input = {"abc","deq","mee","aqq","dkd","ccc"};
-        String[] pattern = {"abb"};
-    }
+        String[] words = {"abc","deq","mee","aqq","dkd","ccc"};
+        String pattern = "abb";
 
+        List<String> ans = new ArrayList();
+        for(String word :words){
+            if(match(word,pattern)){
+                ans.add(word);
+            }
+        }
+        for (String str : ans)
+        {
+            System.out.println(str);
+        }
+
+    }
+    static boolean match(String word, String pattern) {
+        Map<Character, Character> m1 = new HashMap();
+        Map<Character, Character> m2 = new HashMap();
+
+        for (int i = 0; i < word.length(); ++i) {
+            char w = word.charAt(i);
+            char p = pattern.charAt(i);
+            if (!m1.containsKey(w)) {
+                m1.put(w, p);
+            }
+            ;
+            if (!m2.containsKey(p)) {
+                m2.put(p, w);
+            }
+            ;
+            if (m1.get(w) != p || m2.get(p) != w) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
