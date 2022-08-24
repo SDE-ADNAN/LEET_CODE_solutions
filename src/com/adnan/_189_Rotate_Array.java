b4d680232d293de081a31627476cfea9d66f78a4
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-08-24 23:15:10 +0530

    added the code for 189th leetcode problem

--- src/com/adnan/_189_Rotate_Array.java
+++ src/com/adnan/_189_Rotate_Array.java
@@ -1,6 +1,8 @@
 package com.adnan;
 
 
+import java.util.Arrays;
+
 /*
 
 189. Rotate Array
@@ -48,6 +50,25 @@ Could you do it in-place with O(1) extra space?
   */
 public class _189_Rotate_Array {
     public static void main(String[] args) {
+     int[] nums = {1,2,3,4,5,6,7};
+     int k = 3;
+     rotate(nums,k);
+        System.out.println(Arrays.toString(nums));
+    }
 
+    public static void rotate(int[] nums ,int k) {
+        k = k % nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums, k , nums.length-1);
+    }
+    public static void reverse(int[] nums ,int start ,int end){
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
