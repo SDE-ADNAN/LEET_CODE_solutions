55a284d420162a0109f3f3cf42fb4e0e452bc823
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-08-28 22:20:46 +0530

    added the code for the problem 46th

--- src/com/adnan/_46_Permutations.java
+++ src/com/adnan/_46_Permutations.java
@@ -1,6 +1,9 @@
 package com.adnan;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 
 tutorial used: https://www.youtube.com/watch?v=R3Sm9V2OSCo&ab_channel=Codebix
@@ -43,6 +46,30 @@ All the integers of nums are unique.
 public class _46_Permutations {
 
     public static void main(String[] args) {
+int[] nums = {1,2,3};
+        System.out.println(solution(nums));
+    }
 
+    public static List<List<Integer>> solution(int[] nums){
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        backtrack(res, nums,new ArrayList(),visited);
+        return res;
+    }
+
+    static void backtrack(List<List<Integer>> res, int[] nums, List<Integer> curr, boolean[] visited){
+        if(curr.size() == nums.length){
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for(int i=0;i<nums.length; i++){
+            if(visited[i] == true) continue;
+            curr.add(nums[i]);
+            visited[i] = true;
+            backtrack(res,nums,curr,visited);
+            curr.remove(curr.size()-1);
+            visited[i] = false;
+        }
     }
 }
