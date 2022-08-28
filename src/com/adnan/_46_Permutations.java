9c0f1378b5f1dfb8e200b062e51340bed3032cfc
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-08-28 22:04:02 +0530

    added the 46th permutations leetcode problem

---
+++ src/com/adnan/_46_Permutations.java
@@ -0,0 +1,48 @@
+package com.adnan;
+
+
+/*
+
+tutorial used: https://www.youtube.com/watch?v=R3Sm9V2OSCo&ab_channel=Codebix
+
+46. Permutations
+Medium
+
+12579
+
+215
+
+Add to List
+
+Share
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Example 2:
+
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+Example 3:
+
+Input: nums = [1]
+Output: [[1]]
+
+
+Constraints:
+
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+All the integers of nums are unique.
+
+*/
+public class _46_Permutations {
+
+    public static void main(String[] args) {
+
+    }
+}
