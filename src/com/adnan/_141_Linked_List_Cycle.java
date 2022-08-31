917e634bce69dccc11d71eda28ff98858dd6b580
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-08-31 22:14:11 +0530

    added 141th leetcode problem

---
+++ src/com/adnan/_141_Linked_List_Cycle.java
@@ -0,0 +1,26 @@
+package com.adnan;
+
+
+
+public class _141_Linked_List_Cycle {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
+}
