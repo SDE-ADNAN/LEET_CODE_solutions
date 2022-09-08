8073b52381a5f20aebc63b7e274a48b57a817f6e
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-09 00:01:42 +0530

    added explaination for 36th leetcode sum

--- src/com/adnan/_36th_valid_sudoku.java
+++ src/com/adnan/_36th_valid_sudoku.java
@@ -1,8 +1,87 @@
 package com.adnan;
 
+/*
+
+36. Valid Sudoku
+Medium
+
+6308
+
+792
+
+Add to List
+
+Share
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+
+Example 1:
+
+
+Input: board =
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+
+Input: board =
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+Accepted
+859,493
+Submissions
+1,523,467
+
+
+* */
+
+
+
+
 public class _36th_valid_sudoku {
     public static void main(String[] args) {
+        char[][] board = {{'8', '3', '.', '.', '7', '.', '.', '.', '.'}
+                , {'6', '.', '.', '1', '9', '5', '.', '.', '.'}
+                , {'.', '9', '8', '.', '.', '.', '.', '6', '.'}
+                , {'8', '.', '.', '.', '6', '.', '.', '.', '3'}
+                , {'4', '.', '.', '8', '.', '3', '.', '.', '1'}
+                , {'7', '.', '.', '.', '2', '.', '.', '.', '6'}
+                , {'.', '6', '.', '.', '.', '.', '2', '8', '.'}
+                , {'.', '.', '.', '4', '1', '9', '.', '.', '5'}
+                , {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
 
+        System.out.println(isValidSudoku(board));
     }
     // slow solution
 //     public boolean isValidSudoku(char[][] board) {
@@ -22,10 +101,11 @@ public class _36th_valid_sudoku {
 //         }
 //         return true;
 
-//     }
-
+    //     }
+    //
+    //
     // fastest solution
-    public boolean isValidSudoku(char[][] board) {
+    public static boolean isValidSudoku(char[][] board) {
         // Base case...
         if (board == null || board.length != 9 || board[0].length != 9)
             return false;
@@ -79,4 +159,5 @@ public class _36th_valid_sudoku {
         }
         return true;
     }
+
 }
