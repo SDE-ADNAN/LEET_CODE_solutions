ace4024adb278a69106ed48614e0a7858846cbba
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-09-15 23:59:43 +0530

    added more rows to the pascals triangle

--- src/com/adnan/_118_Pascals_Triangle.java
+++ src/com/adnan/_118_Pascals_Triangle.java
@@ -12,6 +12,8 @@ public class _118_Pascals_Triangle {
                             1   1
                           1   2   1
                         1   3   3  1
+                      1   4   6   4  1
+                    1   5   10  10  5  1
 
 
 
